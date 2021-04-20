9a885a336373e6366a57e906009f9793cc0f230e
ranahamood <ranahamood222@gmail.com>
2021-04-20 17:45:02 +0500

    ui-shown-poll-in-home-and-make-it-responsive

--- app/components/pages/home/index.js
+++ app/components/pages/home/index.js
@@ -2,6 +2,7 @@ import { Box, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
 import { EventCalendar } from '../events/calendar';
+import { Poll } from '../../poll';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,9 +30,37 @@ function Home({ eventList, isLoading }) {
       <Grid xs={12} className={classes.root}>
         <Grid xs={12} className={classes.bannerGridSection} />
         <Grid xs={12} className={classes.statsSection}>
-          <Box m={10}>
-            <Box height="50vh" width={[1, 1, 1, 1 / 2]}>
+          <Box
+            m={[2, 2, 2, 10]}
+            display="flex"
+            flexDirection={['column', 'column', 'column', 'row']}
+          >
+            <Box
+              height="62vh"
+              p={2}
+              mx={[0, 0, 0, 20]}
+              width={[1, 1, 1, 1 / 2]}
+            >
               <EventCalendar home eventList={eventList} isLoading={isLoading} />
+            </Box>
+            <Box
+              height="auto"
+              p={2}
+              mx={[0, 0, 0, 20]}
+              width={[1, 1, 1, 1 / 2]}
+            >
+              <Poll
+                name="Biding"
+                description="Will be Fare Biding Will be Fare BidingWill be Fare BidingWill be Fare BidingWill be Fare BidingWill be Fare BidingWill be Fare Biding"
+                firstOption="Ali"
+                firstOptionValue="50"
+                secondOption="Hamza"
+                secondOptionValue="70"
+                thirdOption="Zulifaqar"
+                thirdOptionValue="20"
+                fourthOption="Waseem"
+                fourthOptionValue="20"
+              />
             </Box>
           </Box>
         </Grid>
